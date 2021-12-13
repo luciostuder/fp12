c3b6081b8b5d4331c59a5705213c844dff51ec3d
Brena Lima <brenakslima@gmail.com>
2021-12-13 17:43:18 +0000

    Update functions and tests file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,19 @@ fun main() {
     val numeros = duplica(arrayOf(arrayOf(2, 5), arrayOf(99), arrayOf(3, 4)))
 
     println(numeros.contentDeepToString())
-}
 
+    val numZeros = contaZeros(arrayOf(arrayOf(2, 0), arrayOf(9, 9), arrayOf(0, 4)))
+    println(numZeros)
+
+    val pares: Array<Array<Pair<Int, Int>>> =
+        arrayOf(arrayOf(Pair(1, 2), Pair(2, 1)), arrayOf(Pair(1, 1), Pair(3, 1)), arrayOf(Pair(2, 0), Pair(2, 2)))
+    println(contaParesIguais(pares))
+
+    val valuesString: Array<Array<String>> =
+        arrayOf(arrayOf("a", "b"), arrayOf("c", "d"), arrayOf("e", "f"))
+
+    println(converteParaString(valuesString))
+}
 
 fun getArray2DDeInteiros(): Array<Array<Int>> {
     return arrayOf(arrayOf(2, 5), arrayOf(7, 1), arrayOf(3, 4))
@@ -13,7 +24,7 @@ fun getArray2DDeInteiros(): Array<Array<Int>> {
 fun duplica(matriz: Array<Array<Int>>): Array<Array<Int>> {
     val result = Array(matriz.size) { Array(1) { 0 } } // initial dummy array of array
 
-    for (linha in 0 until matriz.size) {
+    for (linha in matriz.indices) {
         result[linha] = Array(matriz[linha].size) { 0 }
         for (coluna in 0 until matriz[linha].size) {
             result[linha][coluna] = matriz[linha][coluna]
@@ -25,15 +36,42 @@ fun duplica(matriz: Array<Array<Int>>): Array<Array<Int>> {
 
 
 fun contaZeros(numeros: Array<Array<Int>>): Int {
-    return -1
+    var count = 0
+    for (linha in numeros.indices) {
+        for (coluna in 0 until numeros[linha].size) {
+            if (numeros[linha][coluna] == 0) {
+                count++
+            }
+        }
+    }
+    return count
 }
 
 
-fun contaParesIguais(pares: Array<Array<Pair<Int,Int>>>): Int {
-    return -1
+fun contaParesIguais(pares: Array<Array<Pair<Int, Int>>>): Int {
+    var result = 0
+    for (pos in pares) {
+        for (par in pos) {
+            if (par.first == par.second) {
+                result++
+            }
+        }
+    }
+    return result
 }
 
 
 fun converteParaString(matriz: Array<Array<String>>): String {
-    return ""
+    var result = ""
+    for (arr in matriz) {
+        var linha = ""
+        for(str in arr) {
+            linha += "$str,"
+        }
+        linha = linha.substring(0, linha.length - 1) // tirar a ultima virgula
+        result += linha + "\n"
+    }
+    return result
 }
+
+
--- src/test/kotlin/TestTeacherFicha.kt
+++ src/test/kotlin/TestTeacherFicha.kt
@@ -41,19 +41,42 @@ class TestTeacherFicha {
 
     @Test(timeout = 500)
     fun test_03_contaZeros() {
-        fail("TODO")
+        val expected1 = arrayOf(arrayOf(1, 2), arrayOf(3, 4), arrayOf(99, 42))
+
+        assertFalse((contaZeros(expected1) > 1), "Estás a incrementar quando não possui zero!")
+
+        val expected2 = arrayOf(arrayOf(0, 2), arrayOf(3, 0), arrayOf(99, 42))
+
+        assertFalse((contaZeros(expected2) < 0), "Não estás a incrementar quando possui zero!")
+
     }
 
 
     @Test(timeout = 500)
     fun test_04_contaParesIguais() {
-        fail("TODO")
+        val expected1 = arrayOf(arrayOf(Pair(1, 2), Pair(2, 1)), arrayOf(Pair(6, 1), Pair(3, 1)), arrayOf(Pair(2, 0), Pair(4, 2)))
+
+
+        assertFalse((contaParesIguais(expected1) > 1), "Estás a incrementar quando não possui pares!")
+
+        val expected2 = arrayOf(arrayOf(Pair(10, 7), Pair(4, 1)), arrayOf(Pair(3, 4), Pair(3, 3)), arrayOf(Pair(7, 7), Pair(8, 2)))
+
+
+        assertFalse((contaParesIguais(expected2) < 0), "Não estás a incrementar quando possui pares!")
     }
 
 
     @Test(timeout = 500)
     fun test_05_converteParaString() {
-        fail("TODO")
+        val expected1 = arrayOf(arrayOf("a", "b"), arrayOf("c", "d"), arrayOf("e", "f"))
+
+        assertFalse((converteParaString(expected1) == "a,b,\nc,d,\ne,f"), "Não estás a remover a vígula no final de cada linha!")
+
+        val expected2 = arrayOf(arrayOf("a", "b"), arrayOf("c", "d"), arrayOf("e", "f"))
+
+        assertFalse((converteParaString(expected2) == "a,b,c,d,e,f"), "Não estás a quebrar linha!")
+
+
     }
 
 }
